bcd79ed79362fe99d9c63e5542da975878911838
AltahhanAi <a.altahhan@leeds.ac.uk>
2026-04-22 12:02:04 +0100

    wip

--- env/gym/.ipynb_checkpoints/discretised-checkpoint.py
+++ env/gym/.ipynb_checkpoints/discretised-checkpoint.py
@@ -70,7 +70,7 @@ This class returns an index for each state observation.
 
 class GymDiscreteS(GymContS, Discretise):
     def __init__(self, env_id, make=gym.make, feature_indices=None, **kw):
-        GymCont.__init__(self, env_id=env_id, make=make)
+        GymContS.__init__(self, env_id=env_id, make=make)
         Discretise.__init__(self, **kw)  # feature_indices already consumed, won't leak
         self.nS = int(np.prod(self.n_bins))
         self.feature_indices = feature_indices
