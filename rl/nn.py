0a1bde8ac584d017f238168d204988aa2a8d15bd
AltahhanAi <a.altahhan@leeds.ac.uk>
2026-04-22 07:45:36 +0100

    wip

--- rl/nn.py
+++ rl/nn.py
@@ -283,7 +283,7 @@ class nnSplitModel(nnModel):
             
     def forward(self, x):
         for l, layer in enumerate(self.layers[:self.head_idx]):
-            x = F.relu(layer(x)) if l != self.flat_idx else layer(x) 
+            x = F.elu(layer(x)) if l != self.flat_idx else layer(x) 
         self._trunk_out = x
         return self.head1(x), self.head2(x)
 
