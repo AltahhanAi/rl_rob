8d1cc6033a120f7024ebc665ff484089b050aea5
AltahhanAi <a.altahhan@leeds.ac.uk>
2026-04-24 16:21:40 +0100

    Frozen commit for assignment

--- rl/.ipynb_checkpoints/version-checkpoint.py
+++ rl/.ipynb_checkpoints/version-checkpoint.py
@@ -1,5 +1,12 @@
-import subprocess
-def check_version(SHA):
-    out = subprocess.check_output(["git","-C",".","log","-1","--oneline"], text=True).strip()
-    print("you are on the latest version" if out.startswith(SHA) else f"you are not on the forzen version: {out}")
+'''
+This file is for checking the students' local repo version 
+against the frozen repo in git to ensure stability and reproducibility.
 
+'''
+import subprocess, pathlib
+
+def check_version(SHA):
+    repo = pathlib.Path(__file__).parent.parent
+    out = subprocess.check_output(
+        ["git","-C",str(repo),"log","-1","--oneline"], text=True).strip()
+    print("you are on the latest version" if out.startswith(SHA) else f"you are not on the frozen version: {out}")
